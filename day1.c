8cfedf96158b1781c59d4fc8dd0feff6fd461a31
Aditya Choudhary <aadi15082007@gmail.com>
2026-02-01 20:34:23 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n=5;
+    int num,pos;
+    int*arr;
+    arr=(int*)malloc(n*sizeof(int));
+    if(arr == NULL){
+        return 1;
+    }
+    for(int i=0;i<n;i++){
+        printf("Enter %d element of array:",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("ARRAY: ");
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+    printf("Enter number to insert:");
+    scanf("%d",&num);
+    printf("Enter position to insert:");
+    scanf("%d",&pos);
+
+    n++;
+    arr=(int*)realloc(arr,n*sizeof(int));
+
+    for(int i=pos; i<n+1 ; i++){
+        arr[i]=arr[i-1];
+    }
+    arr[pos]=num;
+   printf("ARRAY AFTER INSERTION:");
+   for(int i=0;i<n;i++){
+     printf("%d\t",arr[i]);
+   }
+   free(arr);
+   
+   return 0;
+
+}
